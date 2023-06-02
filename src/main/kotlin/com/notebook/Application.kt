7f0e87928e6172d9ed0e8290b8561ae2dc488087
Uche Powers <uche-victor.powers@thermondo.de>
2023-06-02 13:26:45 +0200

    WIP: init

---
+++ src/main/kotlin/com/notebook/Application.kt
@@ -0,0 +1,12 @@
+package com.notebook
+
+import io.ktor.server.application.*
+import com.notebook.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
---
+++ src/test/kotlin/com/notebook/ApplicationTest.kt
@@ -0,0 +1,5 @@
+package com.notebook
+
+
+class ApplicationTest {
+}
